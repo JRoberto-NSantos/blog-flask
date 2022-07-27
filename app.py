71433f82bda85a326aade103bb02f1d0fd7fb5e7
JRoberto-NSantos <desenv.2022@gmail.com>
2022-07-27 00:32:26 +0000

    Estilização (CSS)

--- app.py
+++ app.py
@@ -1,13 +1,42 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello World!!!"
+posts = [
+    {
+        "title": "First post",
+        "body": "Here comes the text",
+        "author": "Jorns",
+        "created": datetime(2022,7,25)
+    },
+    {
+        "title": "Second post",
+        "body": "Here comes the text",
+        "author": "User 1",
+        "created": datetime(2022,7,26)
+    },
+    {
+        "title": "Third post",
+        "body": "Here comes the text",
+        "author": "User 1",
+        "created": datetime(2022,7,26)
+    },
+    {
+        "title": "Fourth post",
+        "body": "Here comes the text",
+        "author": "User 3",
+        "created": datetime(2022,7,26)
+    },
+    {
+        "title": "Fifth post",
+        "body": "Here comes the text",
+        "author": "User 1",
+        "created": datetime(2022,7,26)
+    },
+]
 
-@app.route("/meucontato")
-@app.route("/contato")    
-def meuContato():
-    return render_template('contato.html')
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
