3da336e793c1881b1761395120e534e40061cf0f
JRoberto-NSantos <desenv.2022@gmail.com>
2022-07-29 23:59:21 +0000

    Versão final do blog Flask

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
+import os
 
 
 app = Flask("hello")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+db_url = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("postgres", "postgresql")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "brigadeiro"
 
@@ -31,7 +33,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author')
 
     def set_password(self, password):
-        self.paassword_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
 
     def check_password(self, password):
@@ -77,15 +79,29 @@ def login():
             username = request.form['username']
             password = request.form['password']
             user = User.query.filter_by(username=username).first()
-            if  user is None or not user.check_paassword(password):
+            if  user is None or not user.check_password(password):
                 flash("Incorrect Username or Password")
                 return redirect(url_for('login'))
             login_user(user)
             return redirect(url_for('index'))
- 
         return render_template("login.html") 
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route("/create", methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        body = request.form['body']
+        try:
+            post = Post(title=title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            flash("Error on create post, try again")
+    return render_template('create.html')
